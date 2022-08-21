826077fcc596f2349d7e2f7fad0a14559521c98c
Laskellyb <anikwenzekelly@gmail.com>
2022-08-21 07:57:23 +0100

    improving the app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from "react";
+import "./countryData"
+import Header from "./components/Header";
+import Main from "./components/Main";
 
 export default function App() {
+
+
+  
   return (
     <div>
       <h1>I am a word</h1>
---
+++ src/countryData.js
@@ -0,0 +1,11 @@
+export default [
+  {
+    name: "Nigeria",
+    blurb: "",
+    capital: "Lagos",
+    region: "West Africa",
+    population: "200 million",
+    currency: "Naira",
+    flag: ""
+  }
+]
