e79004e834b65fc369d578329a14111706d9fbdf
Laskellyb <anikwenzekelly@gmail.com>
2022-08-24 09:30:14 +0100

    removed the border bottom boxshadow for the header

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import React from "react";
 import countryData from "./countryData"
 import Header from "./components/Header";
 import Main from "./components/Main";
+import Filter from "./components/Filter";
 
 export default function App() {
+  // Use state to filter based on search
+  const [filtered, setFiltered] = React.useState(countryData)
+
+  // Main component for display of data
   const allCountryData = countryData.map((data) => {
     return <Main 
           name= {data.name}
